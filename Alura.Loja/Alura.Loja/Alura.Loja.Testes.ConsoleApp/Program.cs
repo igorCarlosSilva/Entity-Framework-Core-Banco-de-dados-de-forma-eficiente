abbbedbe8ff14682013c08aebc6914f6b0173059
Igor Carlos <igor.silva@corebiz.com.br>
2017-09-13 01:33:09 -0300

    Relacionamentos um para um

--- Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,6 +10,32 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+           
+        }
+
+        private static void umParaUm()
+        {
+            var fulano = new Cliente();
+            fulano.Nome = "Fulaninho de tal";
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = 12,
+                Logradouro = "Invalidos street",
+                Complemento = "Sobrado",
+                Bairro = "Centro",
+                Cidade = "Cidade"
+            };
+
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Clientes.Add(fulano);
+                contexto.SaveChanges();
+            }
+        }
+        
+        private static void MuitosParaMuitos()
+        {
             var p1 = new Produto() { Nome = "SUco de laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
             var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
             var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Unidade" };
@@ -33,6 +59,5 @@ namespace Alura.Loja.Testes.ConsoleApp
                 contexto.SaveChanges();
             }
         }
-        
     }
 }
